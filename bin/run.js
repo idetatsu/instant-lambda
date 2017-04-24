312e8404e4cae173c478d2551f1406b56baf5e0b
Tatsuro Ide <idetatsu@gmail.com>
2017-04-24 14:35:54 -0700

    Mock run command.

--- bin/run.js
+++ bin/run.js
@@ -1,3 +1,32 @@
+var fs = require('fs');
+var child_process = require('child_process');
+
 module.exports = run = () => {
-	
+	let currentWorkingDir = process.cwd();
+
+	// read lambda-config.json
+	if(!fs.existsSync(`${currentWorkingDir}/lambda-config.json`)) {
+		console.error('Error: Missing lambda-config.json. Aborting.');
+		process.exit(1);
+	}
+	let lambdaConfigJson = JSON.parse(fs.readFileSync(`${currentWorkingDir}/lambda-config.json`));
+
+	// get the handler method.
+	let handlerFile = lambdaConfigJson.handlerFile ;
+	let handlerMethod = lambdaConfigJson.handlerMethod || 'main';
+	let handler = require(`${currentWorkingDir}/${handlerFile}`)[handlerMethod];
+
+	// read event.json
+	let event = {}
+	if(fs.existsSync(`${currentWorkingDir}/event.json`)) {
+		event = JSON.parse(fs.readFileSync(`${currentWorkingDir}/event.json`));
+	}
+
+	runHandler(handler, event)
+};
+
+runHandler = (handler, event) => {
+	let callback = () => {};
+	let context = {};
+	handler(event, context, callback);
 };
--- global-cli/index.js
+++ global-cli/index.js
@@ -9,21 +9,22 @@ var run = require('../bin/run');
 var pack = require('../bin/pack');
 var deploy = require('../bin/deploy');
 
-var commands = argv._;
-if (commands.length === 0) {
+if (argv.length === 0) {
 	console.error(
 		'Usage: instalam create <function-name>'
 	);
 	process.exit(1);
 }
 
-if(commands.version) {
-	console.log('instant-lambda version: ' + require('./package.json').version);
+if(argv.version || argv.v) {
+	console.log('instant-lambda version: ' + require('../package.json').version);
 	process.exit();
 }
 
+var commands = argv._;
 switch(commands[0]) {
-	case 'create': {
+	case 'create':
+	case 'c': {
 		if(!commands[1]){
 			console.error(
 				'Error: Missing function name. \n' +
@@ -35,15 +36,18 @@ switch(commands[0]) {
 		create(commands[1]);
 		break;
 	}
-	case 'run': {
+	case 'run':
+	case 'r': {
 		run();
 		break;
 	}
-	case 'pack': {
+	case 'pack':
+	case 'p': {
 		pack();
 		break;
 	}
-	case 'deploy': {
+	case 'deploy':
+	case 'd': {
 		deploy();
 		break;
 	}
@@ -51,5 +55,6 @@ switch(commands[0]) {
 		console.error(
 			'Usage: instalam create <project-name>'
 		);
+		process.exit(1);
 	}
 }
