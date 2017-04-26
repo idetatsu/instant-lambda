7ed467fd9c96b75b63516f696e299cf0a3bb837f
Tatsuro Ide <idetatsu@gmail.com>
2017-04-26 16:23:45 -0700

    Fix conflict in test.js

--- test/test.js
+++ test/test.js
@@ -1,18 +1,8 @@
 const assert = require('assert');
 require('assert-fs')(assert);
 
-<<<<<<< HEAD
 describe('Foo', () => {
 	it('does such and such', () => {
 		assert.equal();
 	});
 });
-=======
-const create = require('../bin/create');
-
-describe('create', () => {
-	it('creates Lambda direcrory', () => {
-		assert.equal()
-	});
-});
->>>>>>> Add mocha and test.js
