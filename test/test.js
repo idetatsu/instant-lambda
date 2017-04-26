c188cf040cccca7548aa5e46ec23cbacdf34d795
Tatsuro Ide <idetatsu@gmail.com>
2017-04-26 07:51:19 -0700

    Add mocha and test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 const assert = require('assert');
 require('assert-fs')(assert);
 
+<<<<<<< HEAD
 describe('Foo', () => {
 	it('does such and such', () => {
 		assert.equal();
 	});
 });
+=======
+const create = require('../bin/create');
+
+describe('create', () => {
+	it('creates Lambda direcrory', () => {
+		assert.equal()
+	});
+});
+>>>>>>> Add mocha and test.js
