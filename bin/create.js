ca2aa3278028fb35940f57b70b513cc87a8c212e
Tatsuro Ide <idetatsu@gmail.com>
2017-05-12 19:42:29 -0700

    Add .gitignore to the template files

--- bin/create.js
+++ bin/create.js
@@ -26,6 +26,7 @@ function create(lambdaName) {
 	copyFromTemplateDirToLambdaDir('app.js', lambdaName);
 	copyFromTemplateDirToLambdaDir('deploy-config.json', lambdaName);
 	copyFromTemplateDirToLambdaDir('event.json', lambdaName);
+	copyFromTemplateDirToLambdaDir('.gitignore', lambdaName);
 	createLambdaConfig(lambdaName);
 	createPackageJson(lambdaName);
 
